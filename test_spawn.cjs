bc0a5033ebc5f9a9344636937c9adb2718f3aca9
tracygibson10-alt <tracygibson10-alt@users.noreply.github.com>
2026-06-08 00:22:06 +0000

    Cleanup: remove sensitive and test files

--- test_spawn.cjs
+++
@@ -1,5 +0,0 @@
-const { spawnSync } = require('child_process');
-const sql = "SELECT 'hello \"world\"' as msg";
-const result = spawnSync('team-db', [sql]);
-console.log('STDOUT:', result.stdout.toString());
-console.log('STDERR:', result.stderr.toString());
--- test_spawn.js
+++
@@ -1,4 +0,0 @@
-const { spawnSync } = require('child_process');
-const sql = "SELECT 'hello \"world\"' as msg";
-const result = spawnSync('team-db', [sql]);
-console.log(result.stdout.toString());
